5c03986cbab310abf9fec263598c1cd030904a85
mridula-shanker <42977368+mridula-shanker@users.noreply.github.com>
2018-10-02 21:39:54 -0400

    Update RectangleMain.java

--- RectangleInput/src/RectangleMain.java
+++ RectangleInput/src/RectangleMain.java
@@ -5,7 +5,7 @@ public class RectangleMain
 
 	public static void main(String[] args) 
 	{
-		Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
 
         System.out.print("Enter Length of the Rectangle : ");
         double length = in.nextDouble();
